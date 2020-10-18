88ec9b305bf2177a474373d425c4fddd0438efc8
Ilia Shevaldin <blacktor666@gmail.com>
2020-10-18 20:41:57 +0300

    Выполненео усложенное задание номер 6

--- JS/game3.js
+++ JS/game3.js
@@ -1,45 +1,60 @@
 'use strict';
 
-const isNumber = function(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
-};
 
-const game = function() {
-    let randomNumber = parseInt(Math.random() * 101);  
-    return function repeat() {
-        //randomNumber - является замыканием функции
+const game = function () {
+    const isNumber = function (n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+    };
+
+    let tryNumber = 10;
+
+    function repeat() {
+        let randomNumber = parseInt(Math.random() * 101);
         let userNumber = prompt('Введите значение число');
-
-        /*Если для userNumber присвоить +prompt, то сравнение при отмене (userNumber === null)
-        не будет отыгрываться и следовательно программа не будет работать корректрно */ 
-
-        // провереям введено число или строка
-        if(isNumber(userNumber)) { //если число, то выполняем код с 13 по 25 строку
-            // приводим userNumber к числу
-            if (+userNumber > randomNumber) { //scope Closure
+        let question = function () {
+            return confirm('Попытки закончились! Хотите поробовать еще?');
+        };
+        if (tryNumber > 1) {
+            if (userNumber === null) {
+                alert('Игра окончена');
+            } else if (+userNumber > randomNumber) { //scope Closure
                 alert('Ваше число больше');
+                tryNumber--;
+                console.log(tryNumber);
                 return repeat();
-            // приводим userNumber к числу
-            } else if (+userNumber < randomNumber){
+            } else if (+userNumber < randomNumber) {
                 alert('Ваше число Меньше');
+                tryNumber--;
+                console.log(tryNumber);
                 return repeat();
-            } else {
-                if (confirm('Вы угадали! Сыграем еще?')) {
-                    game()();
-                }else {
-                    alert('Вы завершили игру');
+            } else if (!isNumber(userNumber)) {
+                alert('Введи число');
+                return repeat();
+            } else if (+userNumber === randomNumber) {
+                alert('Вы угадали!');
+                let userNumberTwo = confirm('Поиграем еще?');
+                if (userNumberTwo === true) {
+                    game();
+                } else {
+                    alert('Игра окончена');
+                }
+            } else if (tryNumber === 1 && +userNumber > randomNumber) {
+                alert('Загаданное число меньше');
+                if (question() === false) {
+                    alert('Игра окончена');
+                } else {
+                    game();
+                }
+            } else if (tryNumber === 1 && +userNumber < randomNumber) {
+                alert('Загаданное число меньше');
+                if (question() === false) {
+                    alert('Игра окончена');
+                } else {
+                    game();
                 }
             }
-        } else if (userNumber === null) { //если нажата отмена - завершаем игру
-            alert('Вы завершили игру');
-        } else {
-            return repeat(); //если введено число запускаем игру повторно
         }
-        
-    };
-    
+    }
+    repeat();
 };
-console.dir(game());
-game()(); 
-/*первый возов функции запускает саму функцию game, второй вызов позволяет войти
-внутрь функции и отработать описанные условия (запустится функция repeat)*/
+game();
